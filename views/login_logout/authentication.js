38e3ab5102f96445548c558c9918afe94e66c3fa
tirth91022 <tirth.raval.2024@gmail.com>
2024-03-29 15:54:11 +0530

    All task done

---
+++ views/login_logout/authentication.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+const authentication = async(req,res,next) =>{
+    console.log("hello vijay");
+    if(req.cookies.token){
+        next();
+        
+    }
+    else{
+        res.redirect("/sign_in");
+    }
+}
+module.exports = authentication;
